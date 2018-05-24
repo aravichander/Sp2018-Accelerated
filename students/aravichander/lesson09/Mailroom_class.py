3a4711ec1b61e8d30120c312c3360b841d91767c
Aarthi Ravichander <aravich@uw.edu>
2018-05-24 00:46:59 -0700

    Refactored code to work with class instances

--- students/aravichander/lesson09/Mailroom_class.py
+++ students/aravichander/lesson09/Mailroom_class.py
@@ -6,32 +6,32 @@ import unittest
 
 #If a function works with a single donor, it will be in this class
 class Donor:
-	def __init__(self,Name,donations = None): #add default parameters for donations
-		self.Name = Name
-		self.donations = [] if donations is None else donations 
+	def __init__(self,name,donations = None): #add default parameters for donations
+		self._name = name
+		self._donations = [] if donations is None else donations 
 
-	def add_donations(self,donation):
-		 self.donations.append(donation)
+	def add_donation(self,donation):
+		self._donations.append(donation)
 
 	@property
-	def Name(self):
-		return self.Name
+	def name(self):
+		return self._name
 
 	@property
 	def donations(self):
-		return self.donations
+		return self._donations
 
 	@property
-	def sum_donation(self, donations):
-		return sum(self.donations)
+	def sum_donation(self):
+		return sum(self._donations)
 
 	@property
 	def number_of_donations(self,donations):
-		return len(self.donations)
+		return len(self._donations)
 
 	@property
 	def donation_average(self,donations):
-		return average(self.donation)
+		return average(self._donation)
 
 	# def __repr__(self):
 	# 	return "Circle({})".format(self.radius) 
@@ -40,50 +40,64 @@ class Donor:
 #If a function works with multiple donors, it will be in this class
 class Donor_db:
 	def __init__(self):
-		self.donors = {}
+		self._donors = {}
 
 	def add_donor(self,donor):
-		self.donors[donor.name.lower()] = donor
+		self._donors[donor.name.lower()] = donor
 
-	def total_donors(self,donor_name):
-		return self.donors[donor_name.lower()].sum_donation
+	def all_donors_donations(self):
+		total_sum = 0
+		for x,v in self.donors.items():
+			total_sum = total_sum + sum(v)
+		return total_sum
 
-	 def get_donor(self, donor_name):
-        return self.donors[donor_name.lower()]
+	def get_donor(self, donor_name):
+	 	return self._donors[donor_name.lower()]
+
+	def print_Db(self):
+		for name,donor_obj in self._donors.items(): 
+			print("\n{} donated individual donations of ${} for a total of ${}".format(name,donor_obj.donations,donor_obj.sum_donation))
+
+	def get_sorted_donors(self): 
+		return sorted(self._donors)
 
 	@property
 	def numberofdonors(self):
-		return len(self.donors)
-
-
-
-
+		return len(self._donors)
 
 #################################################################################
 
-Db = Donor_db()
 
 def create_db():
-	#a = Donor("Bill Gates")
-	#add donations to Donor using method
-	Donors["Bill Gates"] = [10000,12000]
-	Donors["Jeff Bezos"]=[50]
-	Donors["Mark Zuckerberg"] = [500,600,700]
-	Donors["Paul Allen"] = [250,350,450]
-	Donors["King of Siam"] = [200,250]
+	a = Donor("Bill Gates")
+	a.add_donation(10000)
+	a.add_donation(12000)
+	b = Donor("Jeff Bezos")
+	b.add_donation(50)
+	c = Donor("Mark Zuckerberg")
+	c.add_donation(500)
+	c.add_donation(600)
+	c.add_donation(700)
+	d = Donor("Paul Allen")
+	d.add_donation(250)
+	d.add_donation(350)
+	d.add_donation(450)
+	e = Donor("King of Siam")
+	e.add_donation(200)
+	e.add_donation(250)
+	donor_db = Donor_db()
+	donor_db.add_donor(a)
+	donor_db.add_donor(b)
+	donor_db.add_donor(c)
+	donor_db.add_donor(d)
+	donor_db.add_donor(e)
+	return donor_db
 
-def get_Db():
-	return Db 
-	
-
-def print_Db():
-	for x,v in Db.items(): #modify to get proper name of donor
-		print("\n{} donated ${}".format(x,v)) 
-
-def thankall():
-	for x in Db:
-		totaldonated = sum(Donors[x]) #call property of donor that calculates total donations
-		letterofthanks(x,totaldonated)
+def thankall(db):
+	for name in db.get_sorted_donors():
+		donor = db.get_donor(name)
+		totaldonated = donor.sum_donation
+		letterofthanks(name,totaldonated)
 
 def letterofthanks(donorname,totaldonated): #Potentially create method for letterofthanks within donor class
 	#To add sum of donations
@@ -94,54 +108,54 @@ def letterofthanks(donorname,totaldonated): #Potentially create method for lette
 	f.close()
 	print("\nLetter of thanks has been saved to hard disk as text file under "+str(filename))
 
-def sumdonor(thankdonor): #redundant - refactor to remove this
-	return sum(Donors[thankdonor])
+# def sumdonor(thankdonor): #redundant - refactor to remove this
+# 	return sum(Donors[thankdonor])
 
-def searchdonors(thankdonor):
+def searchdonors(thankdonor,db):
 		try:
-			print(Donors[thankdonor])
-			totaldonated = sumdonor(thankdonor)
+			#print(db[thankdonor])
+			donor = db.get_donor(thankdonor)
+			totaldonated = donor.sum_donation
 			letterofthanks(thankdonor,totaldonated)
 		except KeyError:
 			print("\nThat donor doesn't exist")
 
 
-def newdonation():
+def newdonation(db):
 	newdonor = input("Input name of new donor\n")
 	while len(str.strip(newdonor)) == 0:
 		newdonor = input("Invalid input, enter name of new donor\n")
 	newdonor = str.strip(newdonor)
-	Donors[newdonor] = []
+	newdonor_object = Donor(newdonor)
 	donationsdone = False
-	newdonations = []
 	while donationsdone is False:	
 		donation = input("Input donations or hit enter when done\n")
 		if donation == "":
 			donationsdone = True 
 		else:
 			try:
-				newdonations.append(float(donation))
+				newdonor_object.add_donation(float(donation))
 			except ValueError:
 				print("Enter number or hit enter - no letters!")
 	#print("Donations is/are {}".format(newdonations))
-	Donors[newdonor] = newdonations
-	printdonors()
+	db.add_donor(newdonor_object)
+	db.print_Db()
 
-def thanks():
+def thanks(db):
 	thankdonor = input("\nPlease enter full name of donor you'd like to thank or list to view donors\n")
 	if thankdonor == "list":
-		printdonors()
-		thanks()
+		db.print_Db()
+		thanks(db)
 	else:
-		searchdonors(thankdonor)
+		searchdonors(thankdonor,db)
 
-def report():
-	sorteddonors = sorted(Donors)
+def report(db):
+	sorteddonors = db.get_sorted_donors()
 	print(sorteddonors)
 
 
 def mainloop():
-	createlist()
+	donor_database = create_db()
 	print("Introducing Mailroom!")
 	done = False
 	while done is False:
@@ -151,13 +165,13 @@ def mainloop():
 		print("response was,",response)
 		if response =="1":
 			print("\nYou have chosen the 'Thank' option")
-			thanks()
+			thanks(donor_database)
 		elif response == "2":
-			report()
+			report(donor_database)
 		elif response == "3":
-			thankall()
+			thankall(donor_database)
 		elif response == "4":
-			newdonation()
+			newdonation(donor_database)
 		else:
 			print("\nSee you later!")
 			done = True 
@@ -165,3 +179,5 @@ def mainloop():
 
 if __name__ == "__main__":
 	mainloop()
+
+
